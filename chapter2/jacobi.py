9c8396666da772539123210de8fdf02bfbc532f2
João M. Campagnolo <jota.campagnolo@gmail.com>
2018-09-25 14:43:28 -0300

    Error fixed, new print iteration method.

--- chapter2/jacobi.py
+++ chapter2/jacobi.py
@@ -6,12 +6,15 @@ def jacobi(A, B, X):
     # Create the submatrices:
     D = np.diag(A) # Creates a vector with the main Diagonal from A matrix.
     R = A - np.diagflat(D) # Subtract the main Diagonal from A matrix.
-    # Iterate N times:
-    for i in range(MAX_ITER):
+    # Jacobi's iteration:
+    ITER = 0 # Iterations counter.
+    while(abs(M[-1] - M[-2]) > ERRO and ITER < MAX_ITER):
         X = (B - np.dot(R, X)) / D
+        M.append(sum(abs(X)))
+        ITER += 1
+        printIteration("GAUSS JACOBI", X, M, ITER)
     # Return the result vector:
     return X
 
 # MAIN:
-X = jacobi(A, B, X)
-printResult(X, "JACOBI")
+jacobi(A, B, X)
--- chapter2/lib.py
+++ chapter2/lib.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as pl
 from datetime import datetime
 
 # Constants:
-ERRO = 1e-4
-MAX_ITER = 1000
+ERRO = 1e-4 # Absolute error to stop the program.
+MAX_ITER = 1000 # Maximum of iterations to stop the program.
 A = np.array([ # A Matrix:
     [4, -1, 0, -1, 0, 0, 0, 0, 0],
     [-1, 4, -1, 0, -1, 0, 0, 0, 0],
@@ -19,9 +19,21 @@ A = np.array([ # A Matrix:
 ])
 B = np.array([50, 40, 90, 10, 0, 50, 140, 130, 180]) # Independent terms.
 X = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1]) # Guess vector.
+M = [np.inf, sum(abs(X))] # Vector that store all the results vector module.
 
 # Function:
-def printResult(X, method):
-    print("[ ### {:^30} ### ]".format(method + " METHOD"))
-    for i in range(len(A[0])):
-        print("X" + str(i+1) + " = " + str(X[i]))
+def printIteration(method, X, M, ITER):
+    print("+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+")
+    print("| {:^48} |".format(method + " METHOD"))
+    print("+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+")
+    for i in range(len(X)):
+        print("| {:^12} | {:>33} |".format("X" + str(i+1), X[i]))
+        if (i+1) < len(X):
+            print("+--------------------------------------------------+")
+    print("+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+")
+    print("| {:^12} | {:>33} |".format("ITERATION", ITER))
+    print("+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+")
+    print("| {:^12} | {:>33} |".format("MODULE", M[-1]))
+    print("+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+")
+    print("| {:^12} | {:>33} |".format("ERROR", abs(M[-1]-M[-2])))
+    print("+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+\n")
